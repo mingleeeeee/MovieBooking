fd8ef777c7d4bd6ab098e9f20df2299e41832014
Wayne <waynekuo86122@gmail.com>
2017-06-14 19:03:49 +0800

    0614

---
+++ demo/src/main/java/com/example/controller/MemberController.java
@@ -0,0 +1,60 @@
+package com.example.controller;
+
+import com.example.dao.MemberDAO;
+
+import java.sql.SQLException;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.dao.MovieDAO;
+import com.example.dao.TicketDAO;
+import com.example.dao.TicketTypeDAO;
+import com.example.dao.TimeSlotDAO;
+import com.example.dao.TransactionDAO;
+import com.example.entity.Member;
+import com.example.entity.Movie;
+import com.example.entity.ProductCategory;
+import com.example.entity.ShoppingCart;
+import com.example.entity.Ticket;
+import com.example.entity.Ticket_type;
+import com.example.entity.Time_slot;
+import com.example.entity.Transaction;
+
+public class MemberController {
+	
+	@Autowired
+	MemberDAO memberdao;
+	
+	@RequestMapping(value = "/registration" , method = RequestMethod.GET)
+	public ModelAndView openFormCreate(){
+		ModelAndView model = new ModelAndView("/registration");
+		model.addObject("Member",new Member());
+		return model;
+	}
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView movieDone(@Valid @ModelAttribute("Member") Member member,  BindingResult bindingResult)
+			 {
+		ModelAndView model;
+		if (bindingResult.hasErrors()) {
+			
+	         model = new ModelAndView("registraction");	           	         
+	         return model;
+	       }
+		model=new ModelAndView("redirect:/user/index");
+		model.addObject(member);	
+		memberdao.save(member);
+		return model;
+		
+	}
+
+}
